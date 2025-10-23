19e263b367784dce5c044a54a97f3523ea21afcd
wijdane64 <abdelmalekwijdane.1997@gmail.com>
2025-10-23 23:15:51 +0100

    deuxeme commit

---
+++ app/Http/Controllers/EmployerController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\employer;
+use App\Http\Requests\StoreemployerRequest;
+use App\Http\Requests\UpdateemployerRequest;
+
+class EmployerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreemployerRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(employer $employer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(employer $employer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateemployerRequest $request, employer $employer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(employer $employer)
+    {
+        //
+    }
+}
---
+++ resources/views/employé.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WorkUnity</title>
+</head>
+<body>
+  <table>
+    <tr>
+    <th>nom</th>
+    <th>prénom</th>
+    <th>emai</th>
+    <th>poste</th>
+  </tr>
+  </table>  
+</body>
+</html>
